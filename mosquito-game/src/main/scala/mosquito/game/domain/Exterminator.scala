c5e9b684e95191a0d306bf33bc6eabb7405ed27f
Tairone de Oliveira <55848422+dadpig@users.noreply.github.com>
2025-04-15 13:05:33 -0700

    Merge pull request #13
    * mosquito: creating game
    
    * mosquito: run game

--- mosquito-game/src/main/scala/mosquito/game/domain/Exterminator.scala
+++ mosquito-game/src/main/scala/mosquito/game/domain/Exterminator.scala
@@ -1,3 +1,32 @@
 package mosquito.game.domain
 
-case class Exterminator()
+case class Exterminator(var horizontal: Int,var vertical: Int) {
+  
+  val VALUE = 1 //default value for exterminator
+
+  def move(board: Array[Array[Int]]): Unit = {
+    // Move exterminator in a specific pattern
+    if (horizontal < 99 && vertical == 0) {
+      horizontal += 1
+    } else if (horizontal == 99 && vertical < 99) {
+      vertical += 1
+    } else if (horizontal > 0 && vertical == 99) {
+      horizontal -= 1
+    } else if (horizontal == 0 && vertical > 0) {
+      vertical -= 1
+    }
+    if(checkCollision(board)){
+      
+    }
+    board(horizontal)(vertical) = VALUE
+    
+  }
+
+  def checkCollision(board: Array[Array[Int]]): Boolean = {
+    board(horizontal)(vertical) == -1
+  }
+
+  def getHorizontal: Int = horizontal
+
+  def getVertical: Int = vertical
+}
--- mosquito-game/src/main/scala/mosquito/game/domain/Game.scala
+++ mosquito-game/src/main/scala/mosquito/game/domain/Game.scala
@@ -1,3 +1,70 @@
 package mosquito.game.domain
 
-case class Game()
+import scala.collection.mutable.ArrayBuffer
+
+object Game {
+  var exterminator: Exterminator = Exterminator(99, 0)
+  var mosquitos: ArrayBuffer[Mosquito] = ArrayBuffer.fill(5)(Mosquito(scala.util.Random.nextInt(100), scala.util.Random.nextInt(100)))
+  var board: Array[Array[Int]] = Array.fill(100, 100)(0)
+
+  def start(): Unit = {
+    //setup
+    for(mosquito <- mosquitos) {
+      board(mosquito.horizontal)(mosquito.vertical) = mosquito.VALUE
+    }
+    board(exterminator.horizontal)(exterminator.vertical) = exterminator.VALUE
+    print(matrix2String(board))
+    // Start the game loop
+    while (true) {
+      var it = mosquitos.iterator
+      while (it.hasNext) {
+        val mosquito = it.next()
+        mosquito.move(board)
+        if (mosquito.getMoveCount % 5 == 0 && mosquito.isAlive) {
+          mosquitos.addOne(mosquito.reproduce(board))
+        }
+        board(mosquito.horizontal)(mosquito.vertical) = mosquito.VALUE
+      }
+      exterminator.move(board)
+      if (exterminator.checkCollision(board)) {
+        exterminator.horizontal = exterminator.getHorizontal
+        exterminator.vertical = exterminator.getVertical
+        //kill mosquito
+        for (mosquito <- mosquitos) {
+          if(mosquito.isAlive && mosquito.horizontal == exterminator.horizontal && mosquito.vertical == exterminator.vertical) {
+            mosquito.isAlive = false
+            board(mosquito.horizontal)(mosquito.vertical) = 0 //release position
+          }
+        }
+        board(exterminator.horizontal)(exterminator.vertical) = exterminator.VALUE
+    }
+
+    /*  for(mosquito <- mosquitos) {
+        if (mosquito.isAlive) {
+          mosquito.move(board)
+          if (mosquito.getMoveCount % 5 == 0 && mosquito.isAlive) {
+            mosquitos.addOne(mosquito.reproduce(board))
+          }
+          board(mosquito.horizontal)(mosquito.vertical) = mosquito.VALUE
+        }
+        exterminator.move(board)
+        if (exterminator.checkCollision(board)) {
+          exterminator.horizontal = exterminator.getHorizontal
+          exterminator.vertical = exterminator.getVertical
+          mosquito.isAlive = false
+          board(exterminator.horizontal)(exterminator.vertical) = exterminator.VALUE
+        }
+      }*/
+      print(matrix2String(board))
+      Thread.sleep(1000)
+    }
+  }
+
+
+  def matrix2String[T](matrix: Array[Array[T]]): String =
+    matrix.map(_.mkString(" ")).mkString("\n")
+
+
+
+  def getDeadMosquitos: Int = mosquitos.count(!_.isAlive)
+}
