cbccef78ee05103433882063d885d3c9349d12c2
tlopesdeoliveira <tlopesdeoliveira@lendingclub.com>
2025-05-02 12:40:18 -0700

    akka first look

--- scala-akka-example/src/main/scala/akka/bank/actor/BankAccountActor.scala
+++ scala-akka-example/src/main/scala/akka/bank/actor/BankAccountActor.scala
@@ -2,7 +2,7 @@ package akka.bank.actor
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import akka.bank.model.{BankAccountMessage, Deposit, PrintBalance, Withdraw}
+import akka.bank.model.{BankAccountMessage, Deposit, GetBalance, PrintBalance, Withdraw}
 
 class BankAccountActor {
 
@@ -10,6 +10,9 @@ class BankAccountActor {
   def behavior(balance: Int): Behavior[BankAccountMessage] = Behaviors.receiveMessage {
     case Deposit(amount) => behavior(balance + amount)
     case Withdraw(amount) => behavior(balance - amount)
+    case GetBalance(replyTo) =>
+      replyTo ! balance
+      Behaviors.same
     case PrintBalance =>
       println(s"balance = $balance")
       behavior(balance)
