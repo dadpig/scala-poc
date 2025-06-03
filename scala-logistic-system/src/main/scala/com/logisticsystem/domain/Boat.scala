19e1a809687193e55a82fc181ebd5f7c64ca6334
tlopesdeoliveira <tlopesdeoliveira@lendingclub.com>
2025-06-03 14:24:33 -0700

    start logic of price based on price of oil and weather

--- scala-logistic-system/src/main/scala/com/logisticsystem/domain/Boat.scala
+++ scala-logistic-system/src/main/scala/com/logisticsystem/domain/Boat.scala
@@ -4,12 +4,10 @@ import com.logisticsystem.business.{GasOil, Weather}
 
 import scala.annotation.targetName
 
-class Boat(name: String, distance: Int, capacity: Int, price: Double, gasOil: GasOil, weather: Weather) extends Transport(name, distance, capacity, price, gasOil) {
+class Boat(name: String, distance: Int, capacity: Int, price: Double, gasOil: GasOil, weather: Weather) extends Transport(name, distance, capacity, price, gasOil, weather) {
 
 
   def reachMaxCapacity: Boolean = capacity < 10000
 
   def minCapacity: Boolean = capacity > 2000
-
-
-  
+}
--- scala-logistic-system/src/main/scala/com/logisticsystem/domain/Transport.scala
+++ scala-logistic-system/src/main/scala/com/logisticsystem/domain/Transport.scala
@@ -13,11 +13,14 @@ abstract class Transport (val name: String, val distance: Int, val capacity: Int
     val costPerUnit = {
       //logic to calculate cost per unit based on gasOil price and weather conditions
       if (gasOil.isExpensive) {
-        if (weather.isStormy) 1.5 * gasOil.price
-        else gasOil.price
+        if (weather.isStormy) 1.8 * gasOil.price
+        else if (weather.isWindy) 1.5 * gasOil.price
+        else gasOil.price * 1.2
+        
       } else {
-        if (weather.isStormy) 1.2 * gasOil.price
-        else gasOil.price * 0.8
+        if (weather.isStormy) 1.4 * gasOil.price
+        else if (weather.isWindy) gasOil.price * 1.2
+        else gasOil.price
       }
     }
     distance * costPerUnit
