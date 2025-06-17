a9de129ee398dcee8b1163e478c6d4c0824f2406
Tairone de Oliveira <55848422+dadpig@users.noreply.github.com>
2025-06-17 14:05:53 -0700

    Update ScalaLoops.scala

--- scala-playground/src/main/scala/ScalaLoops.scala
+++ scala-playground/src/main/scala/ScalaLoops.scala
@@ -1,5 +1,5 @@
 @main 
-def useWhile(text: String*) = {
+def useWhile(text: String*):Unit = {
   var index = 0;
   while (index !=0) {
     if(index !=0){
@@ -12,13 +12,22 @@ def useWhile(text: String*) = {
 }
 useWhile("Test", "of", "useWhile", "loop")
 
-def useForEach(text: String*) = {
+def useForEach(text: String*):Unit = {
   text.foreach(txt => println(txt))
 }
 useForEach("Test", "of", "useForEach", "loop")
 
-def useConsiseFor(text: String*) = {
+def useConsiseFor(text: String*):Unit = {
   for txt <- text do
     println(txt)
 }
 useConsiseFor("Test", "of", "useConsiseFor", "loop")
+
+def useForFilter():Unit = {
+  val filesHere = (new java.io.File(".")).listFiles
+  
+  for file <- filesHere do 
+     if file.getName.endsWith("scala") then
+         println(file)
+}
+useForFilter()
