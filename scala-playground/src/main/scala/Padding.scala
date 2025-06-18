661de0309f7cd52069d010a1a47e70bbc51cfce6
Tairone de Oliveira <55848422+dadpig@users.noreply.github.com>
2025-06-18 06:47:18 -0700

    Update Padding.scala

--- scala-playground/src/main/scala/Padding.scala
+++ scala-playground/src/main/scala/Padding.scala
@@ -1,11 +1,13 @@
 object Padding:
 
+   @main
    def padLines(text: String, minWidth: Int): String = {
      val padLines = 
         for line <- text.linesIterator yield
            padLine(line, minWidth)
      padLines.mkString("\n")
    }
+
    private def padLine(line: String, minWidth: Int): String {
      if line.length >= minWidth then line
      else line + " "+ * (minWidth - line.length)
