1119867150d67ed573f6ed6fa1794ef950909d33
tlopesdeoliveira <tlopesdeoliveira@lendingclub.com>
2025-10-24 15:40:32 -0700

    DPK03_06 use foldLeft to traverse map

---
+++ engineering-problems/src/main/scala/lookup/DPK03_06.scala
@@ -0,0 +1,35 @@
+package lookup
+
+import scala.annotation.tailrec
+
+object DPK03_06 {
+
+  def lookup(map: Map[String, String], key: String): Option[String] = {
+    map.foldLeft(Option.empty[String]) { (acc, pair) =>
+      acc.orElse {
+        val (k, v) = pair
+        if (k == key || v == key) Some(v) else None
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sampleMap = Map("John" -> "John@internet.com", "Bob"-> "Bob@internet.com", "Charlie" -> "Charlie@internet.com")
+    val keyToLookup = "John"
+    val result = lookup(sampleMap, keyToLookup)
+
+    result match {
+      case Some(name) => println(s"Found: $name")
+      case None => println(s"No entry found for key: $keyToLookup")
+    }
+
+    //filter for values
+    val valueToLookup = "John@internet.com"
+    val resultVal = lookup(sampleMap, valueToLookup)
+    result match {
+      case Some(name) => println(s"Found Value: $name")
+      case None => println(s"No entry found for value: $keyToLookup")
+    }
+  }
+
+}
