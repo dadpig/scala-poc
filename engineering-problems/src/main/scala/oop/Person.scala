65e022294fe13f01e0acf69563b9dd76daf9f7e0
tlopesdeoliveira <tlopesdeoliveira@lendingclub.com>
2025-11-18 17:38:16 -0800

    DPK15_02 working with OOP refact

--- engineering-problems/src/main/scala/oop/Person.scala
+++ engineering-problems/src/main/scala/oop/Person.scala
@@ -1,33 +1,32 @@
 package oop
 
-class Person (private val name: String, val age: Int, val friends: List[Person]) {
+class Person (private val name: String, val age: Int, var friends: List[Person]) {
 
 
-  //Refactor the code so you dont allow the same friend to be added twice.
   def addFriend(name: String): Unit = {
-    if (!friends.exists(_.name == name))
-      this.friends.updated(friends.length, new Person(name, 0, List()))
+    if (!this.friends.exists(_.name == name)) {
+      this.friends = this.friends :+ new Person(name, 0, List())
+    }
   }
 
-  def removeFriend(name: String): Person = {
+  def removeFriend(name: String): Unit = {
     val updatedFriends = friends.filterNot(_.name == name)
-    new Person(this.name, this.age, updatedFriends)
+    this.friends = updatedFriends
+
   }
 
-  def getFriends(): List[String] = {
-    friends.map(_.name)
+  private def listFriendsNames(): List[String] = {
+    this.friends.map(_.name)
   }
 
-  override def toString: String = s"Person(name=$name, age=$age, friends=[${getFriends().mkString(", ")}])"
-
-
-
-
+  override def toString: String = {
+    s"Person(name=$name, age=$age, friends=${listFriendsNames()})"
+  }
 }
-
 object PersonApp {
   def main(args: Array[String]): Unit = {
     val person = new Person("John", 30, List())
+    println(person)
     person.addFriend("Paul")
     person.addFriend("George")
     person.addFriend("Ringo")
