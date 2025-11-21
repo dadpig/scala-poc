62b4ec6edb61e63909f82eb38bb9455043b3b7a6
tlopesdeoliveira <tlopesdeoliveira@lendingclub.com>
2025-11-21 15:54:19 -0800

    DPK10_06 using match case

---
+++ engineering-problems/src/main/scala/reduce/DPK10_06.scala
@@ -0,0 +1,24 @@
+package reduce
+
+
+object DPK10_06 {
+
+
+  def reduce[T, U](list: List[T], func: (U, T) => U, initial: U): U = {
+    list match {
+      case Nil => initial
+      case _ => list.foldLeft(initial)(func)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = List(1, 2, 3, 4, 5)
+    val sumResult = reduce(input, (acc: Int, x: Int) => acc + x, 0)
+    println(s"Sum of $input: $sumResult")
+
+    val productResult = reduce(input, (acc: Int, x: Int) => acc * x, 1)
+    println(s"Mult of $input: $productResult")
+  }
+
+
+}
