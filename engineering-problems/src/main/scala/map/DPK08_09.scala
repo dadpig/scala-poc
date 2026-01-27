0b7dee0bc5763ad993512c14c4f0c4bc04f32deb
tlopesdeoliveira <tlopesdeoliveira@lendingclub.com>
2026-01-26 17:22:36 -0800

    DPK08_09 using stack principle

---
+++ engineering-problems/src/main/scala/map/DPK08_09.scala
@@ -0,0 +1,31 @@
+package map
+
+import scala.annotation.tailrec
+
+object DPK08_09 {
+
+  private def mapList[T, U](list: List[T], func: T => U): List[U] = {
+
+    @tailrec
+    def  unStackRec(remaining: List[T], acc: List[U]): List[U] = {
+      remaining match {
+        case Nil => acc
+        case head :: tail => unStackRec(tail, acc :+ func(head))
+      }
+    }
+
+    unStackRec(list, Nil)
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = List(1, 2, 3, 4, 5)
+    val result = mapList(input, (x: Int) => x * 2)
+    println(s"$input double values: $result")
+
+    val inputStr = List("a", "b", "c", "d")
+    val resultStr = mapList(inputStr, (s: String) => s.toUpperCase)
+    println(s"$inputStr capitalize letters: $resultStr")
+  }
+}
+
